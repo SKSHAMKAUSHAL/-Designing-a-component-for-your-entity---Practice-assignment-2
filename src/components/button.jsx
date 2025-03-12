bdb6b5ca70b5956f43a6431f9d588f25ad410c39
Sksham Kaushal <skshamkaushal@gmail.com>
2025-03-12 10:09:06 +0530

    changes

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,1 +1,19 @@
 // write button card here
+import { useState } from "react";
+
+function Button(){
+    const [colour, setColor] = useState('blue')
+
+    return(
+        <div>
+            <button style={{
+                backgroundColor: colour
+            }} onClick={()=>{
+                setColor((e) => e === 'crimson' ? 'blue' : 'crimson')
+            }}>Click Here !</button>
+        </div>
+    );
+}
+
+
+export default Button
--- src/components/productcard.jsx
+++ src/components/productcard.jsx
@@ -1,1 +1,30 @@
 // write product card here
+import Button from "./button";
+
+
+function Productcard(){
+    const productImage='https://m.media-amazon.com/images/I/71s0HTuXpuL._SX679_.jpg';
+    const productName ='MUSCLE BLAZE WHEY PROTEIN';
+    const price='7999'
+
+
+    return(
+        <div>
+            <p> {productName} </p>
+
+        <img style={{
+            width:"200px"
+        }} src={productImage}/>
+            <Button/>
+            <p>{price}</p>
+        </div>
+    );
+}
+
+
+export default Productcard;
+// card should display:- 
+// - product image
+// - product name 
+// - price
+// - View product button
